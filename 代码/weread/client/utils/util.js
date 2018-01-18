6c0665cd5859368683a9fd5156fea17311e50bd5
zy <yzhang@tcomchina.com>
2018-01-18 16:04:46 +0800

    c

--- 代码/weread/client/utils/util.js
+++ 代码/weread/client/utils/util.js
@@ -17,26 +17,26 @@ const formatNumber = n => {
 
 // 显示繁忙提示
 var showBusy = text => wx.showToast({
-    title: text,
-    icon: 'loading',
-    duration: 10000
+  title: text,
+  icon: 'loading',
+  duration: 10000
 })
 
 // 显示成功提示
 var showSuccess = text => wx.showToast({
-    title: text,
-    icon: 'success'
+  title: text,
+  icon: 'success'
 })
 
 // 显示失败提示
 var showModel = (title, content) => {
-    wx.hideToast();
+  wx.hideToast();
 
-    wx.showModal({
-        title,
-        content: JSON.stringify(content),
-        showCancel: false
-    })
+  wx.showModal({
+    title,
+    content: JSON.stringify(content),
+    showCancel: false
+  })
 }
 
 module.exports = { formatTime, showBusy, showSuccess, showModel }
