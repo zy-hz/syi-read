836b32700146feee19fa59f297f686505d53ee63
zy <yzhang@tcomchina.com>
2018-02-08 14:19:58 +0800

    c

--- 代码/weread/client/pages/memberTaskList/memberTaskList.js
+++ 代码/weread/client/pages/memberTaskList/memberTaskList.js
@@ -1,5 +1,5 @@
 var wxutil = require('../../utils/z-util-wx.js')
-const { org, task } = require('../../weread.js')
+const { org, util } = require('../../weread.js')
 
 // 页面函数，传入一个object对象作为参数
 Page(createPageObject());
@@ -9,10 +9,16 @@ function createPageObject() {
   var obj = new Object();
 
   obj.data = {
-    tasks: {},
+    HiddenNoDataPanel: true,
+
+    // 列表中最小的任务编号
+    MinTaskId: -1,
+    // 任务列表
+    Tasks: {}
   };
 
   obj.onLoad = onLoad;
+  obj.onReachBottom = onReachBottom;
 
   return obj;
 }
@@ -21,28 +27,53 @@ function createPageObject() {
  * 载入页面
  */
 function onLoad(options) {
-  var thePage = this;
 
-  org.checkUserAuth({
-    afterNoAuth() {
-      wx.reLaunch({ url: '/pages/welcome/welcome' })
-    },
-
-    afterHasAuth() {
-      doLoadTaskList(thePage)
-    },
-
-    fail(err) {
-      wxutil.showModel('检查用户授权失败', err)
-      console.log('检查用户授权失败', err)
-    }
-
+  wx.setNavigationBarTitle({
+    title: "任务列表",
   })
+
+  this.setData({ HiddenNoDataPanel: false })
+
+  // 载入任务列表
+  doLoadTasks(this);
 }
 
 /**
- * 载入用户列表
+ * 页面滚到到底部了
  */
-function doLoadTaskList(thePage) {
-
+function onReachBottom() {
+  console.log("on bottom");
 }
+
+/**
+ * 载入任务
+ */
+function doLoadTasks(thePage) {
+  // 载入的参数
+  var pms = {
+    Limit: 20,
+    MinTaskId: thePage.data.MinTaskId,
+  }
+
+  wxutil.showLoading();
+  org.getTasks({
+    pms,
+
+    success(result) {
+      wxutil.hideLoading();
+
+      // 载入任务列表
+      const { Tasks } = result.data
+      if (Tasks != null && Tasks.length > 0) {
+        thePage.setData({ Tasks, HiddenNoDataPanel: true })
+      }
+
+      console.log(Tasks);
+    },
+    fail(error) {
+      wxutil.showModel('载入任务列表失败', error);
+      console.log('载入任务列表失败', error);
+    }
+  })
+}
+
