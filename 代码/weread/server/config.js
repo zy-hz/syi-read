97348689165ee5fe6eb4765ee4462f533b07a22a
张易 <moon.tcomchina@gmail.com>
2018-02-25 18:31:12 +0800

    c

--- 代码/weread/server/config.js
+++ 代码/weread/server/config.js
@@ -21,7 +21,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'wxdb3c0e700520efe2',
+        pass: 'you app id',
         char: 'utf8mb4'
     },
 
